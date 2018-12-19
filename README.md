4234272ee89026a74da9b41ae4599bd87156ba8b
Jeff Nabors <jeffnaborsdev@gmail.com>
2018-12-19 12:36:22 -0500

    Cleaning up code, finalized timeline funcitionality.

--- README.md
+++ README.md
@@ -3,12 +3,21 @@ Horus is an easy-to-use Twitter client in the terminal, built in Node.js with Vo
 
 ## How to set up
 
-Once Horus is running, type `init` to begin the setup process to enter your API keys from Twitter. You will need to sign up [here](https://developer.twitter.com/en/apps),
+Once Horus is running, type `setup` to begin the setup process to enter your API keys from Twitter. You will need to sign up [here](https://developer.twitter.com/en/apps),
 signing up with your Twitter account and creating a new App in order to generate your API keys. 
+
+## Usage
+
+### `timeline [tweets]`
+
+This displays your home timeline, showing the oldest descending. You can optionally add a number of tweets to view, with the default being 10 for terminal readability. 
 
 ### Libraries Used
 
 Commander.js
+
 Chalk
+
 Inquirer.js
+
 Twit
--- bin/index.js
+++ bin/index.js
@@ -11,6 +11,7 @@ const twitter = require('../lib/twitter')
 // Helpers
 const clearConsole = process.stdout.write('\033c') // clears console to run program
 const help = chalk.yellow
+const logError = chalk.red.underline
 const log = console.log
 
 if ( fs.existsSync(`${__dirname}/../.env`) === false ) {
@@ -18,46 +19,71 @@ if ( fs.existsSync(`${__dirname}/../.env`) === false ) {
 }
 
 horus
-  .version('0.0.1')
-  .option('init', 'Set up Twitter API keys for Horus to run.')
-  .option('home', 'Read your home timeline.')
-  .parse(process.argv)
-  
-
-  if(horus.init) {
-    log(help.underline('In order to utilize Horus, you will need to visit https://developer.twitter.com/en/apps to generate your API Keys.'))
+  .command('setup')
+  .alias('-s')
+  .description('Set up Twitter API keys for Horus to run.')
+  .action(() => {
     inquirer.prompt([
-      {
-      type:'input',
-      name:'consumer_key',
-      default:'',
-      message: help('What is your Consumer Key? ')
-  },
-  {
-      type:'input',
-      name:'consumer_secret',
-      default:'',
-      message: help('What is your Consumer Secret? ')
-  },
-  {
-      type:'input',
-      name:'access_token_key',
-      default:'',
-      message: help('What is your Access Token Key? ')
-  },
-  {
-      type:'input',
-      name:'access_token_secret',
-      default:'',
-      message: help('What is your Access Token Secret? ')
-  }]).then(result => {
-      let keys = `CONSUMER_KEY=${result.consumer_key}\nCONSUMER_SECRET=${result.consumer_secret}\nACCESS_TOKEN_KEY=${result.access_token_key}\nACCESS_TOKEN_SECRET=${result.access_token_secret}`
-      fs.writeFile(`${__dirname}/../.env`, keys, err => {
-          if (err) throw err
-      })
-      log(help('Setup is complete. You are now able to access Twitter.'))
+        {
+        type:'input',
+        name:'consumer_key',
+        default:'',
+        message: help('What is your Consumer Key? ')
+    },
+    {
+        type:'input',
+        name:'consumer_secret',
+        default:'',
+        message: help('What is your Consumer Secret? ')
+    },
+    {
+        type:'input',
+        name:'access_token_key',
+        default:'',
+        message: help('What is your Access Token Key? ')
+    },
+    {
+        type:'input',
+        name:'access_token_secret',
+        default:'',
+        message: help('What is your Access Token Secret? ')
+    }]).then(result => {
+        let keys = `CONSUMER_KEY=${result.consumer_key}\nCONSUMER_SECRET=${result.consumer_secret}\nACCESS_TOKEN_KEY=${result.access_token_key}\nACCESS_TOKEN_SECRET=${result.access_token_secret}`
+        fs.writeFile(`${__dirname}/../.env`, keys, err => {
+            if (err) throw err
+        })
+        log(help('Setup is complete. You are now able to access Twitter.'))
+    })
   })
-}
+
+// Twitter timeline
+horus
+  .command('timeline [num]')
+  .alias('-t')
+  .description('Read your home timeline. Optional argument allowed for more or less tweets to be shown, defaulting to 10.')
+  .action( tweets => {
+      if (typeof tweets === 'number') {
+      twitter.getTweets(tweets)
+      } else {
+          log(logError('You did not enter a number of tweets to read.'))
+      }
+  })
+
+horus
+  .command('post <text>')
+  .alias('-p')
+  .description('Post a tweet.')
+  .action(text => {
+      if(text.length > 280) {
+          log(logError('Your tweet is too long. Try to keep it under 280 characters!'))
+      } else {
+          // Post tweet function here
+      }
+  })
+
+// TODO: Command for liking / Re-Tweeting tweets
+
+horus.parse(process.argv)
 
 // TODO: Allow optional arg for user to set own count
 // Command for tweets
